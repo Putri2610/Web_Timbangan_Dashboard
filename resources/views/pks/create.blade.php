0dd75eae18ad7ace40637d148112a2ac4d119da5
Putri2610 <yedrahmaliaput02@gmail.com>
2026-07-14 10:43:00 +0700

    Update create.blade.php

--- resources/views/pks/create.blade.php
+++ resources/views/pks/create.blade.php
@@ -6,19 +6,13 @@
     <title>Tambah PKS</title>
 </head>
 <body>
-
     <h1>Tambah PKS</h1>
-
     <form action="{{ route('pks.store') }}" method="POST">
         @csrf
-
         <label>Nama PKS</label><br>
         <input type="text" name="nama_pks">
-
         <br><br>
-
         <button type="submit">Simpan</button>
     </form>
-
 </body>
 </html>
